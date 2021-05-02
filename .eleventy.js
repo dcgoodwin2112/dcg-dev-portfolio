5e4a194aa7bf3c9c3589ed49219374e393b7453e
Dan Goodwin <dcgoodwin2112@gmail.com>
2021-05-02 10:36:58 -0400

    Add pass through copy for favicon

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("src/images")
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
 
   return {
     dir: {
